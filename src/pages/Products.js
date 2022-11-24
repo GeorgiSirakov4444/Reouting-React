e8a1137626222fa9e81e80e7535f0de07f57ee0b
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-24 11:31:16 +0200

    setup

---
+++ src/pages/Products.js
@@ -0,0 +1,10 @@
+const Products = () => {
+     return <div>
+                <h2>Main products</h2>
+                <h3>Product 1</h3>
+                <h3>Product 2</h3>
+                <h3>Product 3</h3>
+            </div>;
+};
+
+export default Products;
