81e00d5e6c45c1900f32f5396f5f01974e24f8d4
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-26 19:30:41 +0200

    adding NotFoundPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
         </Route>
       </Switch>
     </Layout>
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+    return (
+        <div className="centered">
+            Page Not Found !
+        </div>
+    );
+};
+
+export default PageNotFound;
