1642de998cf8466176138bf5eb6e3159e39fc83b
Georgi Sirakov <georgi.sirakov74@gmail.com>
2022-11-27 12:45:59 +0200

    quotes order

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,27 +1,36 @@
-import { useRef } from 'react';
-
+import { Fragment, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isEntered, setIsEntering] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
   function submitFormHandler(event) {
     event.preventDefault();
-
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
 
     // optional: Could validate here
-
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const formFocusHandler = () => {
+    setIsEntering(true);
+  };
+
+  const finishedEnteringFunction = () => {
+    setIsEntering(false);
+  };
+
   return (
+    <Fragment>
+      <Prompt when={isEntered} message={(location) => 'Dont Leave !'}/>
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={formFocusHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +46,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishedEnteringFunction} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
